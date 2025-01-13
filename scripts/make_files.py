01782236ef686b0f9ca536d1853e05ed9d8e2860
pinkfish <pinkfish@users.noreply.github.com>
2025-01-12 20:19:09 -0800

    Update makefile to incluide mmu flag.

--- scripts/make_files.py
+++ scripts/make_files.py
@@ -29,10 +29,10 @@ with open("generate.makefile", "w") as mfile:
     for d in data:
         # Frog
         mfile.write("release/{0}/{1}.3mf: output/{0}__{1}.scad {0}.scad\n".format(d.basename, d.module))
-        mfile.write("\t-mkdir -p release/{0}\n\t../scripts/colorscad/colorscad.sh -f -p $(SCAD) -i $< -o $@ -- -d output/{0}__{1}.deps -D FROM_MAKE=1\n\n".format(d.basename, d.module))
+        mfile.write("\t-mkdir -p release/{0}\n\t../scripts/colorscad/colorscad.sh -f -p $(SCAD) -i $< -o $@ -- -d output/{0}__{1}.deps -D FROM_MAKE=1 -D MMU=1\n\n".format(d.basename, d.module))
         # Extra frogs
         mfile.write("release/{0}/{1}.stl: output/{0}__{1}.scad {0}.scad\n".format(d.basename, d.module))
-        mfile.write("\t-mkdir -p release/{0}\n\t$(SCAD) -m make -o $@ -d output/{0}__{1}.deps $< -D FROM_MAKE=1\n\n".format(d.basename, d.module))
+        mfile.write("\t-mkdir -p release/{0}\n\t$(SCAD) -m make -o $@ -d output/{0}__{1}.deps $< -D FROM_MAKE=1 -D MMU=0\n\n".format(d.basename, d.module))
         # Create the scad file.
         scad_script = "include <../{0}.scad>\n{1}();".format(d.basename, d.module)
         file_data = ""
